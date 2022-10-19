2b06558bb7a3274e95463705a1d128bd5c28eb34
Lucas Gomez <lucasmichaelgomez@gmail.com>
2022-10-19 00:19:11 -0400

    added objects

--- minigrid/core/constants.py
+++ minigrid/core/constants.py
@@ -21,17 +21,9 @@ IDX_TO_COLOR = dict(zip(COLOR_TO_IDX.values(), COLOR_TO_IDX.keys()))
 
 # Map of object type to integers
 OBJECT_TO_IDX = {
-    "unseen": 0,
-    "empty": 1,
-    "wall": 2,
-    "floor": 3,
-    "door": 4,
-    "key": 5,
-    "ball": 6,
-    "box": 7,
-    "goal": 8,
-    "lava": 9,
-    "agent": 10,
+    "circle": 0,
+    "triangle": 1,
+    "square": 2,
 }
 
 IDX_TO_OBJECT = dict(zip(OBJECT_TO_IDX.values(), OBJECT_TO_IDX.keys()))
